856c898f166bf97ad6ebbdad1988c435b118890c
hbrz6 <15007460815@163.com>
2024-05-11 12:55:42 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    if (0 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft) && 0 == maqueen.readPatrol(maqueen.Patrol.PatrolRight)) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+    } else if (1 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft) && 0 == maqueen.readPatrol(maqueen.Patrol.PatrolRight)) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    } else if (0 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft) && 1 == maqueen.readPatrol(maqueen.Patrol.PatrolRight)) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    }
 })
